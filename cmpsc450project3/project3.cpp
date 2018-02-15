85f8c8cb76910226e8b3111afbfafc7536ddf09a
Raj Desai <rajdesai@Rajs-MacBook-Pro.local>
2018-02-15 09:11:53 -0500

    for loop

--- cmpsc450project3/project3.cpp
+++ cmpsc450project3/project3.cpp
@@ -1,7 +1,10 @@
 #include <omp.h>
 #include <stdio.h>
-static long num_steps = 1000000000;
+static long num_steps = 10000;
+int num=10000;
+int ar[num];
 double step;
+int finalsum=0;
 #define NUM_THREADS 3
 // define some space between our summing variables to
 // alleviate false sharing
@@ -13,6 +16,9 @@ int main(void)
 	double pi;
 	// step will be shared (read) among all threads
 	step = 1.0 / (double) num_steps;
+	for(int p=0;p<num;p++){
+		ar[p]=rand()%10;
+	}
 	omp_set_num_threads(NUM_THREADS);
 	// sum will be shared among all threads
 	// each thread will write into a separate array element
@@ -29,14 +35,14 @@ int main(void)
 		i_nthreads = omp_get_num_threads();
 		// limit writing to numThreads to a single thread
 		if (i_ID == 0) i_numThreads = i_nthreads;
+
 		for (i = i_ID; i < num_steps; i+=i_nthreads)
 		{
-			x = (i+0.5) * step;
-			d_sum[i_ID][0] = d_sum[i_ID][0] + 4.0 / (1.0 + x*x);
+			finalsum+=ar[i];
 		}
 	}
 	// finish the summation by summing the individual results of the threads
-	for (int i = 0; i < i_numThreads; i++)
-		pi += d_sum[i][0] * step;
-	printf("PI = %0.9f\n", pi);
+	//for (int i = 0; i < i_numThreads; i++)
+		//pi += d_sum[i][0] * step;
+	printf("FinalSum = %0.9f\n", finalsum);
 }
